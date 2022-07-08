6ab1b986c1ac1be43e1b0571863c82e7a413a752
Eric Martin <ermapes2@gmail.com>
2022-07-09 00:10:31 +0700

    resolve main pages & toolbar

---
+++ app/src/main/java/com/fractaldev/literaku/BackToolbarActivity.kt
@@ -0,0 +1,15 @@
+package com.fractaldev.literaku
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+
+class BackToolbarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_back_toolbar)
+    }
+}
---
+++ app/src/main/java/com/fractaldev/literaku/PanduanActivity.kt
@@ -0,0 +1,18 @@
+package com.fractaldev.literaku
+
+import android.content.Intent
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+
+class PanduanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_panduan)
+    }
+}
