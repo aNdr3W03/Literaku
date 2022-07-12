067e6e356d07f55f0c82dd8b27a5934b5c6231de
Eric Martin <ermapes2@gmail.com>
2022-07-12 11:20:05 +0700

    resolve UI

--- app/src/main/java/com/fractaldev/literaku/RiwayatActivity.kt
+++ app/src/main/java/com/fractaldev/literaku/RiwayatActivity.kt
@@ -1,7 +1,9 @@
 package com.fractaldev.literaku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,12 +14,21 @@ class RiwayatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_riwayat)
+        setToolbar()
 
         rvRiwayat = findViewById(R.id.rvRiwayat)
         rvRiwayat.setHasFixedSize(true)
 
         list.addAll(listBuku)
         showRecyclerList()
+    }
+
+    fun setToolbar() {
+        val settingBtn = findViewById<ImageButton>(R.id.settingBtn)
+        settingBtn.setOnClickListener {
+            val moveIntent = Intent(this@RiwayatActivity, SettingActivity::class.java)
+            startActivity(moveIntent)
+        }
     }
 
     private val listBuku: ArrayList<Buku>
---
+++ app/src/main/java/com/fractaldev/literaku/Utils.kt
@@ -0,0 +1,9 @@
+package com.fractaldev.literaku
+
+object Utils {
+    // Object untuk fungsi umum yang dipakai dibanyak class
+    // Sementara
+    fun dummySum(a: Int, b: Int): Int {
+        return a + b
+    }
+}
