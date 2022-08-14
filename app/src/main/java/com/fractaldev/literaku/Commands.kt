9c6d54e5985b22f4bd044fbc540582579d4b3c22
Eric Martin <ermapes2@gmail.com>
2022-08-14 16:42:41 +0700

    resolve voice command & simplify code (using activity binding)

---
+++ app/src/main/java/com/fractaldev/literaku/Commands.kt
@@ -0,0 +1,61 @@
+package com.fractaldev.literaku
+
+object Commands {
+    // All Pages Commands
+    internal var back = listOf<String>("kembali", "balik")
+    internal var backToHome = listOf<String>(
+        "kembali ke halaman utama",
+        "balik ke halaman utama",
+        "kembali ke halaman pertama",
+        "balik ke halaman pertama",
+        "kembali ke halaman awal",
+        "balik ke halaman awal",
+        "kembali ke awal",
+        "balik ke awal"
+    )
+    internal var exit = listOf<String>("keluar", "keluar aplikasi", "tutup aplikasi")
+
+
+    // MainActivity Commands
+    internal var mainGoToPenjelajah = listOf<String>(
+        "pergi ke halaman penjelajah",
+        "buka halaman penjelajah",
+        "akses halaman penjelajah",
+        "pergi ke penjelajah",
+        "buka penjelajah",
+        "akses penjelajah"
+    )
+    internal var mainGoToRiwayat = listOf<String>(
+        "pergi ke halaman riwayat",
+        "buka halaman riwayat",
+        "akses halaman riwayat",
+        "pergi ke riwayat",
+        "buka riwayat",
+        "akses riwayat"
+    )
+    internal var mainGoToKoleksi = listOf<String>(
+        "pergi ke halaman koleksi",
+        "buka halaman koleksi",
+        "akses halaman koleksi",
+        "pergi ke koleksi",
+        "buka koleksi",
+        "akses koleksi"
+    )
+    internal var mainGoToPanduan = listOf<String>(
+        "pergi ke halaman panduan",
+        "buka halaman panduan",
+        "akses halaman panduan",
+        "pergi ke panduan",
+        "buka panduan",
+        "akses panduan"
+    )
+    internal var mainGoToBantuan = listOf<String>(
+        "pergi ke halaman bantuan",
+        "buka halaman bantuan",
+        "akses halaman bantuan",
+        "pergi ke bantuan",
+        "buka bantuan",
+        "akses bantuan",
+        "bukabantuan"
+    )
+}
--- app/src/main/java/com/fractaldev/literaku/PenjelajahActivity.kt
+++ app/src/main/java/com/fractaldev/literaku/PenjelajahActivity.kt
@@ -1,7 +1,11 @@
 package com.fractaldev.literaku
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.view.GestureDetector
 import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
@@ -10,66 +14,151 @@ import android.webkit.WebView
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
+import com.fractaldev.literaku.databinding.ActivityPenjelajahBinding
+import kotlin.math.abs
 
-class PenjelajahActivity : AppCompatActivity() {
-    lateinit var penjelajahSearchField: EditText
-    lateinit var elWebView: WebView
+class PenjelajahActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
+    private lateinit var activityBinding: ActivityPenjelajahBinding
+    private lateinit var gestureDetector: GestureDetector
+
+    private val swipeThreshold = 100
+    private val swipeVelocityThreshold = 100
+
+    companion object {
+        internal const val REQUEST_CODE_STT = 1
+    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_penjelajah)
+        activityBinding = ActivityPenjelajahBinding.inflate(layoutInflater)
+        setContentView(activityBinding.root)
 
-        elWebView = findViewById(R.id.elWebView)
-        setWebView(elWebView)
+        setWebView(activityBinding.elWebView)
+        setSearchField(activityBinding.penjelajahSearchField)
 
-        penjelajahSearchField = findViewById(R.id.penjelajahSearchField)
-        setSearchField(penjelajahSearchField)
+        gestureDetector = GestureDetector(this)
     }
 
-    fun setWebView(element: WebView) {
-        element.getSettings().setLoadsImagesAutomatically(true)
-        element.getSettings().setJavaScriptEnabled(true)
-        element.getSettings().setDomStorageEnabled(true)
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setWebView(element: WebView) {
+        element.settings.loadsImagesAutomatically = true
+        element.settings.javaScriptEnabled = true
+        element.settings.domStorageEnabled = true
 
-        element.getSettings().setSupportZoom(true)
-        element.getSettings().setBuiltInZoomControls(true)
-        element.getSettings().setDisplayZoomControls(false)
+        element.settings.setSupportZoom(true)
+        element.settings.builtInZoomControls = true
+        element.settings.displayZoomControls = false
 
-        element.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY)
-        element.setWebViewClient(LtkWebViewClient(this@PenjelajahActivity))
+        element.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+        element.webViewClient = LtkWebViewClient(this@PenjelajahActivity)
+
+        element.setOnTouchListener(object: OnSwipeTouchListener(this) {
+            override fun onSwipeLeft() {
+                super.onSwipeLeft()
+                Utils.activateVoiceCommand(this@PenjelajahActivity,
+                    REQUEST_CODE_STT
+                )
+            }
+            override fun onSwipeRight() {
+                super.onSwipeLeft()
+                Utils.activateVoiceCommand(this@PenjelajahActivity,
+                    REQUEST_CODE_STT
+                )
+            }
+        })
     }
 
     @SuppressLint("ClickableViewAccessibility")
     fun setSearchField(element: EditText) {
-        element.setOnTouchListener(OnTouchListener { v, event ->
+        element.setOnTouchListener(OnTouchListener { _, event ->
             val DRAWABLE_RIGHT = 2
             if (event.action == MotionEvent.ACTION_UP) {
-                if (event.rawX >= element.getRight() - element.getCompoundDrawables()
-                        .get(DRAWABLE_RIGHT).getBounds().width()
+                if (event.rawX >= element.right - element.compoundDrawables[DRAWABLE_RIGHT].bounds.width()
                 ) {
-                    search(element.getText().toString())
+                    search(element.text.toString())
                     return@OnTouchListener true
                 }
             }
             false
         })
 
-        element.setOnKeyListener(object : View.OnKeyListener {
-            override fun onKey(view: View?, keyCode: Int, keyevent: KeyEvent): Boolean {
-                return if (keyevent.getAction() === KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                    search(element.getText().toString())
-                    true
-                } else false
-            }
-        })
+        element.setOnKeyListener { _, keyCode, keyevent ->
+            if (keyevent.action === KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                search(element.text.toString())
+                true
+            } else false
+        }
 
     }
 
-    fun search(query: String) {
+    private fun search(query: String) {
         var sendQuery = ""
         if (query != "" && query != null) sendQuery = "search?q=filetype%3Apdf+$query"
 
-        elWebView.loadUrl("https://www.google.com/$sendQuery")
+        activityBinding.elWebView.loadUrl("https://www.google.com/$sendQuery")
+    }
+
+    // Gesture Function Override
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        }
+        else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+        return
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+        return
+    }
+
+    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        try {
+            val diffY = e2.y - e1.y
+            val diffX = e2.x - e1.x
+            if (abs(diffX) > abs(diffY)) {
+                if (abs(diffX) > swipeThreshold && abs(velocityX) > swipeVelocityThreshold) {
+                    Utils.activateVoiceCommand(this@PenjelajahActivity,
+                        REQUEST_CODE_STT
+                    )
+                }
+            }
+        }
+        catch (exception: Exception) {
+            exception.printStackTrace()
+        }
+        return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_CODE_STT -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    result?.let {
+                        val recognizedText = it[0]
+                        Utils.executeVoiceCommand(this, recognizedText.lowercase())
+                    }
+                }
+            }
+        }
     }
 }
