72961a203282f8ac862430c8a1c8b310184320e5
Eric Martin <ermapes2@gmail.com>
2022-08-24 20:01:06 +0700

    resolve indexing penjelajah + add CSE

--- app/src/main/java/com/fractaldev/literaku/ApiConfig.kt
+++ app/src/main/java/com/fractaldev/literaku/ApiConfig.kt
@@ -23,5 +23,22 @@ class ApiConfig {
                 .build()
             return retrofit.create(ApiService::class.java)
         }
+
+        fun getApiServiceCSE(): ApiService {
+            val loggingInterceptor = if(BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://www.googleapis.com/customsearch/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
     }
 }
